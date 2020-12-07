54711232871dbfe5c943100498eb560f767796a4
xueguifeng <xueguifeng.zuoyebang.com>
2020-12-07 21:08:59 +0800

    no message

--- Algorithm/src/LeetCode/Tree/MaxPathSum.java
+++ Algorithm/src/LeetCode/Tree/MaxPathSum.java
@@ -7,6 +7,27 @@ public class MaxPathSum {
 
 
     public int maxPathSum(TreeNode root) {
-        return 0;
+        if (root == null) return 0;
+        value(root);
+        return sum;
+    }
+
+    private int sum = Integer.MIN_VALUE;
+    /**
+     * 计算某个节点提供给父类的最大路径
+     *
+     * 核心思想 利用后序遍历找它的left right节点可以提供给它的值
+     * @param node
+     * @return
+     */
+    private int value(TreeNode node) {
+        if (node == null) return 0;
+        // 计算左右节点
+        int lv = Math.max(value(node.left), 0);
+        int rv = Math.max(value(node.right),0);
+        // 父节点跟自己的左右节点和的最大值比较
+        sum = Math.max(node.val + lv + rv, sum);
+        // 当前节点跟自己的子节点能够取到的最大值
+        return node.val + Math.max(lv, rv);
     }
 }
