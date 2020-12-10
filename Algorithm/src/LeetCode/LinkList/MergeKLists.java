6382555aebc96a44b05ff9c0a9e7ebfd656808cb
George <1234@sina.com>
2020-12-10 20:34:31 +0800

    no message

--- Algorithm/src/LeetCode/LinkList/MergeKLists.java
+++ Algorithm/src/LeetCode/LinkList/MergeKLists.java
@@ -2,6 +2,23 @@ package LeetCode.LinkList;
 
 import java.util.PriorityQueue;
 
+/**
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-k-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ */
 public class MergeKLists {
 
     /**
