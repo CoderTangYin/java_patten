016f35d874ac618e3e44374007072dacd613c5e3
xueguifeng <xueguifeng.zuoyebang.com>
2021-05-20 15:49:55 +0800

    no message

--- Algorithm/src/LeetCode/String/AreAlmostEqual.java
+++ Algorithm/src/LeetCode/String/AreAlmostEqual.java
@@ -17,6 +17,30 @@ package LeetCode.String;
 public class AreAlmostEqual {
 
     public boolean areAlmostEqual(String s1, String s2) {
+            if (s1.length() != s2.length()) return false;
+            if (s1 == null || s2 == null) return false;
+            if (s1.equals(s2)) return true;
+            char[] sa = new char[2];
+            char[] ta = new char[2];
+            // 记录不同的次数超过了2 就不会是一次可以调换的了
+            int difCount = 0;
 
+            for (int i=0; i<s1.length(); i++) {
+                char x = s1.charAt(i);
+                char y = s2.charAt(i);
+                if (x != y) {
+                    if (difCount == 2) return false;
+                    // 记录s1 s2的第一第二的不同字符
+                    sa[difCount] = x;
+                    ta[difCount] = y;
+                    difCount += 1;
+                }
+            }
+            return (sa[0] == ta[1] && sa[1] == ta[0]);
+    }
+
+    public static void main(String[] args) {
+        AreAlmostEqual areAlmostEqual = new AreAlmostEqual();
+        System.out.println(areAlmostEqual.areAlmostEqual("bank", "kanb"));
     }
 }
