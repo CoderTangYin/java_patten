a37465d82951ae1239e48ab4efc76f3728d22808
TangYin <>
2021-06-15 09:56:16 +0800

    no message

--- Algorithm/src/LeetCode/Stack/MinStack.java
+++ Algorithm/src/LeetCode/Stack/MinStack.java
@@ -1,15 +1,18 @@
 package LeetCode.Stack;
 
 
+// 利用链表的头插法 
 public class MinStack {
 
     private Node head;
 
     public MinStack() {
+        // 初始化一个头结点为 值为0 最小值执行最大的结点
         head = new Node(0,Integer.MAX_VALUE,null);
     }
 
     public void push(int x) {
+        // 用插入的值跟头结点的最小值比对 取出最小值 创建写的结点 头插到最前边
         head = new Node(x, Math.min(x, head.min), head);
     }
 
